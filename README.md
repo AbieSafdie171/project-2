7470ce7edec3c71e8a1ed7a549b446267116af5c
Abie Safdie <abie@Abies-MacBook-Air.local>
2023-01-30 14:46:33 -0800

    project2

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
-# UOCS322 - Project 2 #
+# UO CS322 - Project 2 #
 
-#Info
+## Info
 Name: Abie Safdie
+ 
 Email: asafdie11@gmail.com
 
 ## Description of Project
 
-description
+Building a web server using Docker and Flask. Displays page if found within the pages/ directory and
+sends a 404 error if file not found and a 403 error if the request contains forbidden characters. Reads
+the port and debug from the credentials file.
 
 
--- web/app.py
+++ web/app.py
@@ -1,14 +1,54 @@
 """
-John Doe's Flask API.
+Abie Safdie's Flask API.
 """
 
-from flask import Flask
+import os
+import configparser
+from flask import Flask, abort, send_from_directory
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "UOCIS docker demo!\n"
+
+def parse_config(config_paths):
+    config_path = None
+    for f in config_paths:
+        if os.path.isfile(f):
+            config_path = f
+            break
+
+    if config_path is None:
+        raise RuntimeError("Configuration file not found!")
+
+    config = configparser.ConfigParser()
+    config.read(config_path)
+    return config
+
+
+config = parse_config(["credentials.ini", "default.ini"])
+PORT_NUM = config["SERVER"]["PORT"]
+DEBUG_MODE = config["SERVER"]["DEBUG"]
+
+
+@app.route("/<string:request>")
+def hello(request):
+    if "~" in request or ".." in request:
+        abort(403)
+    if len(request) < 1:
+        abort(403)
+    try:
+        f = open(f'pages/{request}', 'r')
+        return send_from_directory('pages/', f'{request}'), 200
+
+    except:
+        abort(404)
+@app.errorhandler(404)
+def notFound(e):
+    return send_from_directory('pages/', '404.html'), 404
+
+@app.errorhandler(403)
+def forbidden(e):
+    return send_from_directory('pages/', '403.html'), 403
+
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=DEBUG_MODE, host='0.0.0.0', port=PORT_NUM)
